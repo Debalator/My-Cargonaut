0756c54e13e6ee2d74c7389b5d8e162882b2d7f2
Lars Wächter <lars97.waechter@gmail.com>
2021-12-04 14:09:47 +0100

    Add infrastructure

---
+++ README.md
@@ -0,0 +1,4 @@
+# My-Cargonaut
+
+![Backend](https://github.com/Debalator/My-Cargonaut/actions/workflows/backend.yaml/badge.svg)
+![Frontend](https://github.com/Debalator/My-Cargonaut/actions/workflows/frontend.yaml/badge.svg)
--- my-cargonaut-frontend/src/test.ts
+++ my-cargonaut-frontend/src/test.ts
@@ -8,7 +8,11 @@ import {
 } from '@angular/platform-browser-dynamic/testing';
 
 declare const require: {
-  context(path: string, deep?: boolean, filter?: RegExp): {
+  context(
+    path: string,
+    deep?: boolean,
+    filter?: RegExp
+  ): {
     keys(): string[];
     <T>(id: string): T;
   };
@@ -17,7 +21,7 @@ declare const require: {
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting(),
+  platformBrowserDynamicTesting()
 );
 
 // Then we find all the tests.

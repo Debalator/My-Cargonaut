f1811c41ea476a0c71fe4e8708022815e1ee4562
Luca Feilbach <luca.feilbach@mni.thm.de>
2021-12-09 11:04:16 +0100

    user module CRUD functions implemented

--- my-cargonaut-backend/src/users/users.service.ts
+++ my-cargonaut-backend/src/users/users.service.ts
@@ -5,16 +5,10 @@ import { UpdateUserDto } from './dto/update-user.dto';
 @Injectable()
 export class UsersService {
 
-  private users: CreateUserDto[] = [
-    {
-      username: 'Harald',
-      mail: 'harald@mail.de',
-      password: 'secure'
-    },
-  ]
+  private users: CreateUserDto[] = []
 
   create(user: CreateUserDto) {
-    this.users.push(user);
+    return this.users.push(user);
   }
 
   findAll() {
@@ -25,11 +19,21 @@ export class UsersService {
     return this.users.find(user => user.username === username);
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  update(username: string, updateUserDto: UpdateUserDto) {
+    let user = this.users.find(user => user.username === username)[0];
+    const index = this.users?.findIndex(user);
+
+    user = updateUserDto;
+
+    this.users[index] = user;
+    return user;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  remove(username: string) {
+    const user = this.users.find(user => user.username === username)[0];
+    const index = this.users?.indexOf(user);
+    if (index > -1)
+      return this.users.splice(index, 1);
+    return null;
   }
 }

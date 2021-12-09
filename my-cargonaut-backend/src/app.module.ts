06636b1d0fba0f8a2e2884509953582ab4b38b50
Lars Wächter <lars97.waechter@gmail.com>
2021-12-09 17:34:16 +0100

    Add offers component

--- my-cargonaut-backend/src/app.module.ts
+++ my-cargonaut-backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OffersModule } from './offers/offers.module';
 
 @Module({
-  imports: [],
+  imports: [OffersModule],
   controllers: [AppController],
   providers: [AppService]
 })

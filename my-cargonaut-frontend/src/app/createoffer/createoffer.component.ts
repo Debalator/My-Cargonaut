e81658e3a096b36a675602c91719f1837ea6e282
Julia-Sophie Mühleck <m.julia-sophie@gmx.de>
2021-12-10 12:39:51 +0100

    missing backend connection

--- my-cargonaut-frontend/src/app/createoffer/createoffer.component.ts
+++ my-cargonaut-frontend/src/app/createoffer/createoffer.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {MatSnackBar} from "@angular/material/snack-bar";
 
 
@@ -9,38 +9,78 @@ import {MatSnackBar} from "@angular/material/snack-bar";
   styleUrls: ['./createoffer.component.scss']
 })
 
+// export interface Offer {
+//   start: string,
+//   stop: string,
+//   date: Date,
+//   vehicle: Vehicle["id"],
+//   space: number,
+//   seats: number,
+//   moreInfo: string;
+// }
+//
+// export interface Vehicle {
+//   id: number,
+//   brand: string,
+//   model: string,
+//   seats: number,
+//   space: number;
+// }
 
-export class CreateofferComponent {
+export class CreateofferComponent implements OnInit{
   constructor(private formBuilder: FormBuilder, private snackbar: MatSnackBar) {
 
   }
 
-  checkoutForm = this.formBuilder.group({
-    start: '',
-    stop: '',
-    date: '',
-    vehicle: '',
-    space: '',
-    seats: '',
-    moreInfo: ''
-  });
+  checkoutForm!: FormGroup;
+  // checkoutForm = this.formBuilder.group({
+  //   start: '',
+  //   stop: '',
+  //   date: '',
+  //   vehicle: '',
+  //   space: '',
+  //   seats: '',
+  //   moreInfo: ''
+  // });
 
   //kein bereits vergangenes Datum auswählbar
   minDate = new Date();
 
   sendData(message: string){
     this.snackbar.open(message,'' ,{duration: 2000});
-  }
-
-
-
-  onSubmit(): void {
-    // Process checkout data here
-    // this.items = this.cartService.clearCart();
-    console.warn('Your offer has been submitted', this.checkoutForm.value);
     this.checkoutForm.reset();
   }
 
+  ngOnInit() {
+    this.createForm();
+  }
 
-
+  createForm(){
+    this.checkoutForm = this.formBuilder.group({
+      startInput: '',
+      stopInput: '',
+      dateInput: '',
+      vehicleInput: '',
+      spaceInput: '',
+      seatsInput: '',
+      moreInfoInput: ''
+    })
+  }
+  // public createOffer(offer: Offer) {
+  //   this.httpService.post("http://localhost:9090/...", {
+  //     start: this.checkoutForm.start,
+  //     stop: offer.stop,
+  //     date: offer.date,
+  //     vehicle: offer.vehicle,
+  //     space: offer.space,
+  //     seats: offer.seats,
+  //     moreInfo: offer.moreInfo
+  //   }).subscribe({
+  //     next: (res: any) => {
+  //       console.log(res);
+  //     },
+  //     error: (e: any) => console.error(e),
+  //     complete: () => console.info('complete')
+  //   });
+  // }
 }

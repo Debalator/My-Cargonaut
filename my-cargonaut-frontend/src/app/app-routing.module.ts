88db47223a4e7f3aea5041ae6b99988429b0088a
Luca Feilbach <luca.feilbach@mni.thm.de>
2021-12-15 13:30:17 +0100

    proxy andd routing added

--- my-cargonaut-frontend/src/app/app-routing.module.ts
+++ my-cargonaut-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateofferComponent } from './createoffer/createoffer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "create_offer",
+    component: CreateofferComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

98a4d4f4beea41464f07b4f68a5b4e3388ac2c0d
Julia-Sophie Mühleck <m.julia-sophie@gmx.de>
2022-01-08 16:01:14 +0100

    database bug fixed

--- my-cargonaut-frontend/src/app/createoffer/createoffer.component.ts
+++ my-cargonaut-frontend/src/app/createoffer/createoffer.component.ts
@@ -58,31 +58,31 @@ export class CreateofferComponent implements OnInit {
   //TODO: missing vehicle and more Info, Skalierung Fenstergröße
 
   public sendInput() {
-
-
+    this.api.post("/api/addresses", {
+      zip: this.checkoutForm.value.startZip,
+      city: this.checkoutForm.value.startCity,
+      country: this.checkoutForm.value.startCountry,
+    }).subscribe({
+      next: (res: any) => {
+        console.log(res);
+      },
+      error: (e: any) => console.error('startAddress' + e),
+      complete: () => console.log('startAddress sent'),
+    });
+    this.api.post("/api/addresses", {
+      zip: this.checkoutForm.value.destZip,
+      city: this.checkoutForm.value.destCity,
+      country: this.checkoutForm.value.destCountry,
+    }).subscribe({
+      next: (res: any) => {
+        console.log(res);
+      },
+      error: (e: any) => console.error('destAddress' + e),
+      complete: () => console.log('destAddress sent'),
+    })
     this.api.post("/api/offers", {
-      startAddress: this.api.post("/api/addresses", {
-        zip: this.checkoutForm.value.startZip,
-        city: this.checkoutForm.value.startCity,
-        country: this.checkoutForm.value.startCountry,
-      }).subscribe({
-        next: (res: any) => {
-          console.log(res);
-        },
-        error: (e: any) => console.error('startAddress' + e),
-        complete: () => console.log('startAddress sent'),
-      }),
-      destAddress: this.api.post("/api/addresses", {
-        zip: this.checkoutForm.value.destZip,
-        city: this.checkoutForm.value.destCity,
-        country: this.checkoutForm.value.destCountry,
-      }).subscribe({
-        next: (res: any) => {
-          console.log(res);
-        },
-        error: (e: any) => console.error('destAddress' + e),
-        complete: () => console.log('destAddress sent'),
-      }),
+      startAddress: this.checkoutForm.value.startZip + ' ' + this.checkoutForm.value.startCity + ' ' + this.checkoutForm.value.startCountry,
+      destAddress: this.checkoutForm.value.destZip + ' ' + this.checkoutForm.value.destCity + ' ' + this.checkoutForm.value.destCountry,
       startDate: this.checkoutForm.value.startDate,
       destDate: this.checkoutForm.value.stopDate,
       price: this.checkoutForm.value.priceInput,

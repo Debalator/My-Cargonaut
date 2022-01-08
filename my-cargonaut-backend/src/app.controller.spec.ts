ab8ff9314a8c669b2659216c3ec59df15d1427af
Luca Feilbach <luca.feilbach@mni.thm.de>
2022-01-08 18:49:17 +0100

    tried to fix backend tests

--- my-cargonaut-backend/src/app.controller.spec.ts
+++ my-cargonaut-backend/src/app.controller.spec.ts
@@ -3,15 +3,22 @@ import { Test, TestingModule } from "@nestjs/testing";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { UsersService } from "./users/users.service";
+import {ModuleMocker, MockFunctionMetadata} from 'jest-mock';
 
 describe("AppController", () => {
     let appController: AppController;
+    const moduleMocker = new ModuleMocker(global);
 
     beforeEach(async () => {
         const app: TestingModule = await Test.createTestingModule({
             controllers: [AppController],
             providers: [AppService, UsersService, JwtService],
-        }).compile();
+        }).useMocker(token => {
+            const mockMetadata = moduleMocker.getMetadata(token) as MockFunctionMetadata<any, any>;
+            const Mock = moduleMocker.generateFromMetadata(mockMetadata);
+            return new Mock();
+        })
+        .compile();
 
         appController = app.get<AppController>(AppController);
     });

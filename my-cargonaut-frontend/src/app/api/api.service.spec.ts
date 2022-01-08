e96f1474d7cd8656b826862464502371cf309201
Luca Feilbach <luca.feilbach@mni.thm.de>
2022-01-08 15:23:14 +0100

    frontend ci format fixed

--- my-cargonaut-frontend/src/app/api/api.service.spec.ts
+++ my-cargonaut-frontend/src/app/api/api.service.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient } from '@angular/common/http';
 import { ApiService } from './api.service';
 
 describe('ApiService', () => {
